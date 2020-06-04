d641ad65db88d42d11cf3eb2438cd4894b48abbd
Joris Kuijper <joris.kuijper@gmail.com>
2020-06-04 10:54:15 +0200

    made comaptible with newest variojs version

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ const initVariojs = async () => {
     url: '/animation-data.json',
   });
   
-  const devTools = await import(/* webpackMode: "lazy", webpackChunkName: "variojs-toolbox" */ 'variojs-toolbox');
-  devTools.default.init(variojs);
-  variojs.play();
+  if(process.env.NODE_ENV === 'development') {
+    const devTools = await import(/* webpackMode: "lazy", webpackChunkName: "variojs-toolbox" */ 'variojs-toolbox');
+    devTools.default.init(variojs);
+  }
+
+  variojs.play("bird");
 };
 
 
@@ -19,17 +22,15 @@ const App = () => {
   }, []);
   return (
     <>
-    <div className="logo-holder">
-      <div data-v="cricle-group">
-        <svg data-v="cricles" id="logo" className="logo" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 182.2 182.2">
-          <g>
-            <ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 -54.3585 47.4633)" className="st0" cx="30.11" cy="89.35" rx="30.14" ry="30.09"/>
-            <ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 -80.3445 110.1989)" className="st1" cx="92.85" cy="152.08" rx="30.14" ry="30.09"/>
-            <ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 -21.1103 134.7345)" className="st2" cx="152.08" cy="92.85" rx="30.14" ry="30.09"/>
-            <ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 4.8756 71.9989)" className="st3" cx="89.35" cy="30.11" rx="30.14" ry="30.09"/>
-          </g>
-        </svg>
-      </div>
+
+    <div className="bird-wrapper" data-v="bird-wrapper">
+        <div className="bird" data-v="bird" >
+          <div className="bird bird-bg"></div>
+          <div data-v="bird1" className="bird bird1"></div>
+          <div data-v="bird2" className="bird bird2"></div>
+          <div data-v="bird3" className="bird bird3"></div>
+          <div data-v="bird4" className="bird bird4"></div>
+        </div>
     </div>
     <p className="scroll">scroll down</p>
     </>
